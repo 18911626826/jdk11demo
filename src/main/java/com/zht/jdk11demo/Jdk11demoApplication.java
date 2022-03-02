3f601f8881eb185b9c1798ebea67cd86bfb40b7d
haitao.zhang <18911626826@163.com>
2022-03-03 01:06:15 +0800

    1

---
+++ src/main/java/com/zht/jdk11demo/Jdk11demoApplication.java
@@ -0,0 +1,16 @@
+package com.zht.jdk11demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class Jdk11demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdk11demoApplication.class, args);
+	}
+
+}
